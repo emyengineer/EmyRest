29c130e1eaa68af619c56fbd2e7e44747a4ae2a1
Eman Zaghloul <emyengineer@gmail.com>
2018-06-10 11:50:00 +0200

    revert last modifications
    remove EmyRest part

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`EmyRest/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}`);
   }
 
   /**
